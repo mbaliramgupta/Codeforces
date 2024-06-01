0316ef1b4d12fc0a0b57b48eafada28c5c18c14e
Baliram Gupta <balramgupta7054@gmail.com>
2024-06-01 14:05:16 +0530

    Codeforces pattern question

---
+++ B_Present_from_Lena.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{     int n;
+      cin>>n;
+      for(int i=0; i<=n;i++){
+        for(int j=0;j<n-i;j++){
+            cout<<"  ";
+        }
+        for(int j=0;j<i;j++){
+            cout<< j <<" ";
+        }
+        cout<<i;
+        for(int j=i-1;j>=0;j--){
+            cout<<" "<<j;
+        }
+        cout<<endl;
+      }
+      for(int i=n-1;i>=0;i--){
+        for(int j=0;j<n-i;j++){
+            cout<<"  ";
+        }
+        for(int j=0;j<i;j++){
+            cout<<j<<" ";
+        }
+        cout<<i;
+        for(int j=i-1;j>=0;j--){
+            cout<<" "<<j;
+        }
+        cout<<endl;
+      }
+    return 0;
+}
+
+    
